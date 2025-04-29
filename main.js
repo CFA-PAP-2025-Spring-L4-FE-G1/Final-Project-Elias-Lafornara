add950c562e71656f02daf034848106dce67905e
Kugusu <elias.lafornara@digitalskillsforall.org>
2025-04-29 16:42:30 -0700

    Fixes search pages and adds links to archive items in the each item's title

--- main.js
+++ main.js
@@ -5,62 +5,66 @@ const main = document.querySelector("main")
 let page = 1;
 
 async function search(query) {
-    main.innerHTML = "";
-    let url = "https://archive.org/advancedsearch.php?q=";
-    query = query.replaceAll(" ", "+");
-    url += query + "&output=json&page=" + page;
-    console.log(url)
-    let results = fetch(url);
-    results = await results;
-    results = await results.json();
-    console.log(results.response.docs);
-    results = results.response.docs;
-    for (let result of results) {
-        let div = document.createElement("div");
-        let title = document.createElement("h4");
-        let description = document.createElement("p");
+    if (query != "") {
+        main.innerHTML = "";
+        let url = "https://archive.org/advancedsearch.php?q=";
+        query = query.replaceAll(" ", "+");
+        url += query + "&output=json&rows=50&page=" + page;
+        console.log(url)
+        let results = fetch(url);
+        results = await results;
+        results = await results.json();
+        console.log(results.response.docs);
+        results = results.response.docs;
+        for (let result of results) {
+            let div = document.createElement("div");
+            let title = document.createElement("a");
+            let description = document.createElement("p");
+            
+            div.classList.add("card");
+            div.id = result.identifier;
+            title.classList.add("title");
+            title.href = "https://archive.org/details/" + result.identifier;
+            description.classList.add("description");
+
+            title.textContent = result.title;
+            description.textContent = result.description;
+
+            div.addEventListener('click', () => {
+                div.id == "clicked" ? div.id = "" : div.id = "clicked";
+            });
+
+            div.append(title);
+            div.append(description);
+            main.append(div);
+            
+        }
+        let changePageForm = document.createElement("div");
+        changePageForm.id = "changePageForm";
+        if (page != 1) {
+            let backButton = document.createElement("button");
+            backButton.id = "backButton";
+            backButton.textContent = "Back";
+            backButton.addEventListener('click', () => {
+                page--;
+                search(query);
+            });
+            changePageForm.append(backButton);
+        }
+        if (page < 9999) {
+            let nextButton = document.createElement("button");
+            nextButton.id = "nextButton";
+            nextButton.textContent = "Next";
+            nextButton.addEventListener('click', () => {
+                page++;
+                console.log(query)
+                search(query);
+            });
+            changePageForm.append(nextButton);
+        }
         
-        div.classList.add("card");
-        title.classList.add("title");
-        description.classList.add("description");
-
-        title.textContent = result.title;
-        description.textContent = result.description;
-
-        div.addEventListener('click', () => {
-            div.id == "clicked" ? div.id = "" : div.id = "clicked";
-        });
-
-        div.append(title);
-        div.append(description);
-        main.append(div);
-        
+        main.append(changePageForm);
     }
-    let changePageForm = document.createElement("div");
-    changePageForm.id = "changePageForm";
-    if (page != 1) {
-        let backButton = document.createElement("button");
-        backButton.id = "backButton";
-        backButton.textContent = "Back";
-        backButton.addEventListener('click', () => {
-            page--;
-            search(query);
-        });
-        changePageForm.append(backButton);
-    }
-    if (page < 9999) {
-        let nextButton = document.createElement("button");
-        nextButton.id = "nextButton";
-        nextButton.textContent = "Next";
-        nextButton.addEventListener('click', () => {
-            page++;
-            console.log(query)
-            search(query);
-        });
-        changePageForm.append(nextButton);
-    }
-    
-    main.append(changePageForm);
 }
 
 
